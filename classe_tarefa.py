e7b44a67e89f4cac15e61f5e8436056cfcd1f346
GiovannaCarraro <giovanna.c.oliveira9@aluno.senai.br>
2025-08-01 14:59:31 -0300

    MODIFY: modifiquei

--- classe_tarefa.py
+++ classe_tarefa.py
@@ -8,5 +8,17 @@ class Tarefa:
         self.lista_tarefa.append(tarefa)
 
     def listar_tarefa(self):
+        cont = 0
         for tarefa in self.lista_tarefas:
-            print(tarefa)
+            print(f"{cont} - {tarefa}")
+            cont += 1 
+
+    def excluir_tarefas(self):
+        self.listar_tarefa()
+        tarefa_excluir = int(input("Qual tarefa deseja excluir (escolha pelo numero)"))
+        self.lista_tarefas.pop(tarefa_excluir)
+
+    def concluir_tarefas(self):
+        self.listar_tarefa()
+        tarefa_concluir = int(input("Qual tarefa deseja concluir (escolha pelo numero)"))
+        self.lista_tarefas(tarefa_concluir) = self.lista_tarefas(tarefa_concluir) + "(CONCLUIDO)"
