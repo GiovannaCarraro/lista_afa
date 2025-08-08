a474a0984dc133f4d24b9cb44233a8fe3e6ed1da
GiovannaCarraro <giovanna.c.oliveira9@aluno.senai.br>
2025-08-08 11:28:19 -0300

    MODIFY: modifiquei a lista de afazeres

--- classe_tarefa.py
+++ classe_tarefa.py
@@ -1,24 +1,24 @@
-class Tarefa:
+# class Tarefa:
 
-    def __init__(self):
-        self.lista_tarefas = []
+#     def __init__(self):
+#         self.lista_tarefas = []
 
-    def adicionar_tarefa(self):
-        tarefa = input("Qual tarefa você deseja incluir?")
-        self.lista_tarefa.append(tarefa)
+#     def adicionar_tarefa(self):
+#         tarefa = input("Qual tarefa você deseja incluir?")
+#         self.lista_tarefas.append(tarefa)
+        
+#     def listar_tarefa(self):
+#         cont = 0
+#         for tarefa in self.lista_tarefas:
+#             print(f"{cont} - {tarefa}")
+#             cont += 1 
 
-    def listar_tarefa(self):
-        cont = 0
-        for tarefa in self.lista_tarefas:
-            print(f"{cont} - {tarefa}")
-            cont += 1 
+#     def excluir_tarefas(self):
+#         self.listar_tarefa()
+#         tarefa_excluir = int(input("Qual tarefa deseja excluir (escolha pelo numero)"))
+#         self.lista_tarefas.pop(tarefa_excluir)
 
-    def excluir_tarefas(self):
-        self.listar_tarefa()
-        tarefa_excluir = int(input("Qual tarefa deseja excluir (escolha pelo numero)"))
-        self.lista_tarefas.pop(tarefa_excluir)
-
-    def concluir_tarefas(self):
-        self.listar_tarefa()
-        tarefa_concluir = int(input("Qual tarefa deseja concluir (escolha pelo numero)"))
-        self.lista_tarefas(tarefa_concluir) = self.lista_tarefas(tarefa_concluir) + "(CONCLUIDO)"
+    # def concluir_tarefas(self):
+    #     self.listar_tarefa()
+    #     tarefa_concluir = int(input("Qual tarefa deseja concluir (escolha pelo numero)"))
+    #     self.lista_tarefas(tarefa_concluir) = self.lista_tarefas(tarefa_concluir) + "(CONCLUIDO)"
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from classe_tarefa import Tarefa
-
+# from classe_tarefa import Tarefa
+afazeres = []
 while True:
   #Menu principal
     print("""     _____  
@@ -24,24 +24,36 @@ while True:
         4- REMOVER TAREFA
         5- SAIR
           """)
-    
+        
     tarefa = int(input("Escolha o que deseja realizar:"))
     
     if tarefa == 1:
       print("Você escolheu inserir tarefas") 
+      tarefa_adc = input("Qual tarefa deseja adicionar?")
+      afazeres.append(tarefa_adc)
       
     elif tarefa == 2:
       print("Você escolheu listar tarefas")
-      
+      for tarefa in afazeres:
+        print(tarefa)
+                            
     elif tarefa == 3:
-      print("Você escolheu marcar concluido")
-          
+      print("Você escolheu marcar concluido")     
+      numero = 0
+      for tarefa in afazeres:
+        print(f"{numero} {tarefa}")
+        numero += 1
+        concluido = int(input("Qual das tarefas você quer marcar como concluida:"))
+        afazeres[concluido] = afazeres [concluido] + "✔"
+
     elif tarefa == 4:
       print("Você escolheu remover tarefa")
-      
+      remover = int(input("Qual item você deseja remover?"))
+      afazeres.remove(remover)
+
     elif tarefa == 5:
       print("Você escolheu sair")
-          
-    break
+      break
+    
       
     
